c997fb5707d616a3080603724f74c53996c538c2
Aleksey Monakhov <goshagriboedskii@gmail.com>
2023-04-15 23:48:53 +0300

    upd

--- client/src/pages/Customizer.tsx
+++ client/src/pages/Customizer.tsx
@@ -61,7 +61,7 @@ const Customizer = () => {
         try {
             setGeneratingImg(true);
 
-            const response = await fetch('http://localhost:8080/api/v1/dalle', {
+            const response = await fetch('https://threejs-d8ev.onrender.com/api/v1/dalle', {
                 method: "POST",
                 headers: {
                     "Content-type":"application/json"
